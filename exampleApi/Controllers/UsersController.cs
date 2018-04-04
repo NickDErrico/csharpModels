89d5a51cee8399de13bda9bdfea6c4c67f4014ec
troyamelotte <troyamelotte@gmail.com>
2018-04-04 09:41:21 -0700

    init

---
+++ exampleApi/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using exampleApi.Models;
+using System.Linq;
+
+namespace exampleApi.Controllers
+{
+	[Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+		private readonly UserContext _context;
+
+        public UsersController(UserContext context)
+		{
+            
+			_context = context;
+			
+			_context.Users.Add(new User { Name = "Kevin" });
+			_context.Users.Add(new User { Name = "Jess" });
+			_context.Users.Add(new User { Name = "James" });
+			_context.SaveChanges();
+			
+		}
+		// GET: /<controller>/
+        [HttpGet]
+	    public ActionResult All()
+	    {
+		    return Json(_context.Users);
+	    }
+    }
+}
